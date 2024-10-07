6dfea009a611dc4cac33f983899acc9adfefe4dd
Rodrigo Ribeiro Gomes <rodrigor.gomes@hotmail.cokm>
2024-10-07 12:24:45 -0300

    Added utils and workflow

---
+++ util/publish.ps1
@@ -0,0 +1,43 @@
+﻿<#
+	Publica na PSGalery!
+#>
+[CmdletBinding()]
+param(
+	$ApiKey = $Env:PSGALERY_KEY
+	,[switch]$Publish
+	,[switch]$CheckVersion
+)
+
+$ErrorActionPreference = "Stop";
+
+[string]$ModuleRoot = Resolve-Path "milvus"
+
+
+# Module version!
+if($CheckVersion){
+	# Current version!
+	$LastTaggedVersion = git describe --tags --match "v*" --abbrev=0;
+	
+	if(!$LastTaggedVersion){
+		throw "No version tags!";
+	}
+
+	$TaggedVersion = [Version]($LastTaggedVersion.replace("v",""))
+
+
+	$Mod = import-module $ModuleRoot -force -PassThru;
+
+	if($TaggedVersion -ne $Mod.Version){
+		throw "MILVUS_PUBLISH_INCORRECT_VERSION: Module = $($Mod.Version) Git = $TaggedVersion";
+	}
+}
+
+if($Publish){
+	$PublishParams = @{
+		Path 		= $ModuleRoot
+		NuGetApiKey = $ApiKey
+		Force 		= $true
+		Verbose 	= $true;
+	}
+	Publish-Module -Path $ModuleRoot -NuGetApiKey $ApiKey -Force -Verbose
+}
